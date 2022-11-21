7cbd34ef55cb8c8c4161ac10b29aecd0d1902ebd
Vlad Dyshlyuk <vladdyshlyuk@yandex.ru>
2022-11-21 09:59:21 +0700

    Added gitignore, removed ingored node_modules

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ app.listen(
 )
 
 app.all('*', function (req, res, next) {
-    res.set('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
