abba6d0296594ba6ea2ebcdc2d009292c6eb559b
Vlad Dyshlyuk <vladdyshlyuk@yandex.ru>
2022-11-21 12:29:21 +0700

    CORS configured

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const app = require('express')();
 const PORT = 8080;
-
 var questionsRepository = require('./questionsRepository');
+const cors = require('cors');
+
+app.options('*', cors())
+
+app.all('*', function (req, res, next) {
+    res.set('Access-Control-Allow-Origin', '*');
+    next();
+});
 
 app.listen(
     PORT,
     () => console.log(`it's alive on ${PORT}`)
-)
-
-app.all('*', function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+);
 
 app.get('/questions/:dif?/:num?', function (req, res) {
     res.send(questionsRepository.getRandom(req.params.num || 1, req.params.dif || 1));
