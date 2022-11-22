68f66e8aca40b2403e5e99f4584ce9725dafe3fa
Vlad Dyshlyuk <vladdyshlyuk@yandex.ru>
2022-11-22 17:49:44 +0700

    Still CORS

--- server.js
+++ server.js
@@ -2,24 +2,6 @@ const app = require('express')();
 const PORT = 8080;
 var questionsRepository = require('./questionsRepository');
 
-const cors = require('cors');
-
-const allowedOrigins = ["*"];
-
-app.use(
-    cors({
-        origin: function (origin, callback) {
-            if (!origin) return callback(null, true);
-            if (allowedOrigins.indexOf(origin) === -1) {
-                var msg =
-                    "The CORS policy for this site does not " +
-                    "allow access from the specified Origin.";
-                return callback(new Error(msg), false);
-            }
-            return callback(null, true);
-        }
-    })
-);
 
 app.all('*', function (req, res, next) {
     res.set('Access-Control-Allow-Origin', '*');
